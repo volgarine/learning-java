e1435005f4b69657bf7dea54f6b333022e83d06e
volgarine <volgarine@gmail.com>
2023-05-12 11:45:48 -0600

    commit first

---
+++ src/main/java/NamingConventions.java
@@ -0,0 +1,29 @@
+public class NamingConventions {
+
+// Packages → com.CompanyName
+
+//Classes → NomeDaClass
+
+// Methods → methodName (camelCase)
+
+// Variables → variableName (camelCase)
+
+// Constants → CONSTANT_NAME
+
+// Objects → nameObject (camelCase)
+
+    /*camelCase (pattern for naming variables)
+
+    declare variable using method: first letter lower case and other letters first case
+
+    The CamelCase naming convention helps make code more readable and easier to understand,
+    making it easier for developers to distinguish between different identifiers and understand
+    the meaning of variables and methods. Furthermore, it is a common practice in the programming industry,
+    making code easier to share and collaborate between development team members.
+
+    it is common to use CamelCase to name variables and methods, such as
+    'fullName', 'bankBalance', 'calculateMedia', etc.
+    In other programming languages, such as Python, it is common to use a similar style called snake_case,
+    where words are separated by an underscore (_), such as 'full_name', 'bank_balance', 'calculate_average', etc.
+*/
+}

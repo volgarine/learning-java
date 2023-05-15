fbc8aa9cde16db8369973ab8d3b79ee9b20788ec
volgarine <volgarine@gmail.com>
2023-05-15 16:27:02 -0600

    commit 1

---
+++ src/main/java/LoopFor4Array.java
@@ -0,0 +1,10 @@
+public class LoopFor4Array {
+    public static void main(String[] args) {
+
+        int [] numbers = {1, 2, 3, 4, 5};
+
+        for(int num : numbers){
+            System.out.println(num);
+        }
+    }
+}

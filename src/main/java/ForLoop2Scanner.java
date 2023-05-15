de30c65ff2530413ed45ad41eea90fec3e3e0050
volgarine <volgarine@gmail.com>
2023-05-15 12:47:49 -0600

    commit 1

---
+++ src/main/java/ForLoop2Scanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ForLoop2Scanner {
+    public static void main(String[] args) {
+
+        //Enter 5 numbers (using for) and print their sum
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 5 numbers to sum");
+        int total = 0;
+        for (int i = 1; i < 6; i++){
+            int number = scanner.nextInt();
+            total = total + number;
+        }
+        System.out.println(total);
+
+    }
+}

c2bc04c0ebfcacbe3a3960e91160f50d9b0a0628
volgarine <volgarine@gmail.com>
2023-05-22 12:50:31 -0600

    commit 1

--- src/main/java/ArrayList2.java
+++ src/main/java/ArrayList2.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 public class ArrayList2 {
     public static void main (String[]args){
 
-        ArrayList<Integer> listaDeNumeros = new ArrayList<>(); //create arraylist (always empty)
+        ArrayList<Integer> listOfNumbers = new ArrayList<>(); //create arraylist (always empty)
         Scanner scanner = new Scanner(System.in);//create Scanner (input) from user
 
-        int valorLido = 0; // variable to be checked in WHILE
+        int valueRead = 0; // variable to be checked in WHILE
 
-        while (valorLido  != 6) { // condition: while the value is not 6, ask to add another number
+        while (valueRead  != 6) { // condition: while the value is not 6, ask to add another number
             System.out.println("Between 1 and 10, try to hit the key number: ");
-            valorLido = scanner.nextInt(); // read and save value added by user
-            if (valorLido != 6){ // checks if added is <> 6 to show list of user attempts to hit 6 that are saved in ArrayList
-                listaDeNumeros.add(valorLido);
+            valueRead = scanner.nextInt(); // read and save value added by user
+            if (valueRead != 6){ // checks if added is <> 6 to show list of user attempts to hit 6 that are saved in ArrayList
+                listOfNumbers.add(valueRead);
             }
         }
 
         // loop to scan the ArrayList printing each saved number that was added by the user
-        if(listaDeNumeros.size() ==0){
+        if(listOfNumbers.size() ==0){
             System.out.println("--------------------------------------------------------");
-            System.out.println("Congrats!!! >>>> You got it right the first time.");
+            System.out.println("(6) → You got it right the first time.");
         } else {
             System.out.println("--------------------------------------------------------");
-            System.out.println("You got it, but you tried "+ listaDeNumeros.size() + " times. Check it out:");
-            for (int i = 0; i < listaDeNumeros.size(); i++){
-                System.out.println(listaDeNumeros.get(i));
+            System.out.println("You got it, but you tried "+ listOfNumbers.size() + " times. Check it out:");
+            for (int i = 0; i < listOfNumbers.size(); i++){
+                System.out.println(listOfNumbers.get(i));
             }
         }
     }
